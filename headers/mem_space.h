a752ca812fd3d462d9332f7937bec2589a81ca9c
Marangozova Vania <marangov@im2ag-mandelbrot2.u-ga.fr>
2023-09-20 17:11:58 +0200

    adding all headers for test programs, may be cleaner

---
+++ headers/mem_space.h
@@ -0,0 +1,23 @@
+//------------------------------------------------------------------------------
+// Projet : TP CSE (malloc)
+// Cours  : Conception des systèmes d'exploitation et programmation concurrente
+// Cursus : Université Grenoble Alpes - UFRIM²AG - Master 1 - Informatique
+//------------------------------------------------------------------------------
+
+#ifndef MEM_SPACE_H
+#define MEM_SPACE_H
+
+#include <stdlib.h>
+
+#if defined(DEBUG)
+#define debug(...) fprintf(stderr, __VA_ARGS__)
+#else
+#define debug(...)
+#endif
+
+/** Retourne l'adresse de l'espace mémoire à utiliser dans l'allocateur. **/
+void *mem_space_get_addr(void);
+/** Retourne la taille de l'espace mémoire à utilier dans l'allocateur. **/
+size_t mem_space_get_size(void);
+
+#endif //MEM_SPACE_H
