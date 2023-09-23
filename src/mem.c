120021990d415cd7f2aa992f73a1e12a89fbb290
enzo_nulli <enzo_nulli@ens.univ-artois.fr>
2023-09-24 00:11:18 +0200

    modif mem_init + debut implementation mem_alloc/mem_first_fit

--- src/mem.c
+++ src/mem.c
@@ -18,12 +18,21 @@ typedef struct mem_busy_block_s {
 	size_t taille;
 } mem_busy_block_t;
 
+
+
+// Pointeur de fonction sur une des fit fonctions.
+typedef mem_free_block_t * (*ptr_fit_function)(mem_free_block_t *,size_t);
+
+// Notre structure d'en-tête
 typedef struct allocator_s {
-	void *first_free_block;
-	void *actual_fit_function;
+	mem_free_block_t *first_free_block;
+	ptr_fit_function actual_fit_function;
 	void *debut_mem;
 	size_t taille_tot;
 } allocator_t;
+
+
+
 
 allocator_t* gbl_alloc=NULL;
 
@@ -35,10 +44,17 @@ allocator_t* gbl_alloc=NULL;
  * If already init it will re-init.
 **/
 void mem_init() {
+	// On stocke l'allocateur à l'adresse 0
 	gbl_alloc = mem_space_get_addr();
+
+	// on crée le premier bloc de mémoire libre
 	mem_free_block_t *first = (mem_free_block_t *) (gbl_alloc + 1);
+	first->taille = mem_space_get_size() - sizeof(gbl_alloc);
+	first->ptr_next_free = NULL;
+
+	// On initialise tous les attributs de l'allocateur
 	gbl_alloc->first_free_block = first;
-	gbl_alloc->actual_fit_function = NULL;
+	gbl_alloc->actual_fit_function = &mem_first_fit;
 	gbl_alloc->debut_mem = mem_space_get_addr();
 	gbl_alloc->taille_tot = mem_space_get_size();
 }
@@ -50,8 +66,16 @@ void mem_init() {
  * Allocate a bloc of the given size.
 **/
 void *mem_alloc(size_t size) {
-	//TODO: implement
-	assert(! "NOT IMPLEMENTED !");
+	// TODO: Mettre à jour le premier bloc libre dans gbl_alloc
+	mem_free_block_t *block_address = gbl_alloc->actual_fit_function(gbl_alloc->first_free_block, size + sizeof(mem_busy_block_t));
+	if(block_address){
+		// On crée "l'en-tete" du bloc alloué
+		mem_busy_block_t *busy_block = (mem_busy_block_t *)block_address;
+		busy_block->taille = size;
+
+		// On donne à l'utilisateur l'adresse du bloc alloué (sans l'en-tête du bloc)
+		return (void *)(busy_block + 1);
+	}
     return NULL;
 }
 
@@ -89,17 +113,24 @@ void mem_show(void (*print)(void *, size_t, int free)) {
 // mem_fit
 //-------------------------------------------------------------
 void mem_set_fit_handler(mem_fit_function_t *mff) {
-	//TODO: implement
-	assert(! "NOT IMPLEMENTED !");
+	gbl_alloc->actual_fit_function = mff;
 }
 
 //-------------------------------------------------------------
 // Stratégies d'allocation
 //-------------------------------------------------------------
 mem_free_block_t *mem_first_fit(mem_free_block_t *first_free_block, size_t wanted_size) {
-    //TODO: implement
-	assert(! "NOT IMPLEMENTED !");
-	return NULL;
+    mem_free_block_t *current_block = first_free_block;
+    
+    while (current_block != NULL) {
+        // Vérifie si le bloc courant est suffisamment grand pour la taille demandée
+        if (current_block->taille >= wanted_size) {
+            return current_block;
+        }
+		// Sinon on passe au bloc libre suivant
+        current_block = current_block->ptr_next_free;
+    }
+    return NULL;
 }
 //-------------------------------------------------------------
 mem_free_block_t *mem_best_fit(mem_free_block_t *first_free_block, size_t wanted_size) {
